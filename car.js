ea1ebc0dcca07fd68417440b1aff5d063fe79a71
chase d <chasedai@icloud.com>
2022-07-16 22:25:20 +0800

    collision happens when hitting borders

--- car.js
+++ car.js
@@ -4,35 +4,64 @@ class Car{
         this.x=x;
         this.y=y;
         this.width=width;
-        this.heigh=height;
+        this.height=height;
+        this.controls= new Controls();
 
 
+        
         this.speed=0;
         this.acceleration=0.002;
 
         this.maxSpeed=10;
 
         this.angle=0;
-
-        this.controls= new Controls();
+        
         this.#addKeyboardListeners();
     }
 
-    update(){
-        this.#move();
-    }
+    // update(){
+    //     this.#move();
+    // }
 
-
-    #addKeyboardListeners(){
-        document.onkeydown=(event)=>{
-            switch(event.key){
-                case "ArrowUp":
-                    this.acceleration+=0.01;
-                    break;
-            }
+    update(roadBorders){
+        if(!this.damaged){
+            this.#move();
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders);
         }
     }
 
+    #assessDamage(roadBorders){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #createPolygon(){
+        const points=[];
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan2(this.width,this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        });
+        return points;
+    }
 
     #move(){
         if(this.controls.forward){
@@ -64,19 +93,46 @@ class Car{
 
     draw(ctx){
         // ctx.save();
-
-        ctx.translate(this.x,this.y);
-        ctx.rotate(-this.angle);
-
+        if(this.damaged){
+            ctx.fillStyle="gray";
+        }else{
+            ctx.fillStyle="black";
+        }
         ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.heigh/2,
-            this.width,
-            this.heigh
-        );
+        ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
+        for(let i=1;i<this.polygon.length;i++){
+            ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
+        }
         ctx.fill();
         // ctx.restore();
+    }
+
+
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.controls.left = true;
+                    break;
+                case "ArrowRight":
+                    this.controls.right = true;
+                    break;
+                case "ArrowUp":
+                    console.log('up!!');
+                    this.acceleration+=0.01;
+                    break;
+            }
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.controls.left = false;
+                    break;
+                case "ArrowRight":
+                    this.controls.right = false;
+                    break;
+            }
+        }
     }
 
 }
--- controls.js
+++ controls.js
@@ -5,33 +5,8 @@ class Controls{
         this.left=false;
         this.right=false;
         this.test=false;
-        this.#addKeyboardListeners();
     
     }
-
-    #addKeyboardListeners(){
-        document.onkeydown=(event)=>{
-            switch(event.key){
-                case "ArrowLeft":
-                    this.left = true;
-                    break;
-                case "ArrowRight":
-                    this.right = true;
-                    break;
-            }
-        }
-        document.onkeyup=(event)=>{
-            switch(event.key){
-                case "ArrowLeft":
-                    this.left = false;
-                    break;
-                case "ArrowRight":
-                    this.right = false;
-                    break;
-            }
-        }
-    }
-
 
 
 }
